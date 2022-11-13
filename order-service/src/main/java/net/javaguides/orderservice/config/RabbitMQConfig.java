02c1c6dd6e8fc126ebbfb9391e9aaee53d4af767
Marcelo Ungaretti <marcelo.ungaretti@gmail.com>
2022-11-13 14:24:21 -0300

    fim da aula 33

--- order-service/src/main/java/net/javaguides/orderservice/config/RabbitMQConfig.java
+++ order-service/src/main/java/net/javaguides/orderservice/config/RabbitMQConfig.java
@@ -15,15 +15,26 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.queue.order.name}")
     private String orderQueue;
 
+    @Value("${rabbitmq.queue.email.name}")
+    private String emailQueue;
+
     @Value("${rabbitmq.exchange.name}")
     private String exchange;
 
     @Value("${rabbitmq.binding.routing.key}")
     private String orderRoutingKey;
 
+    @Value("${rabbitmq.binding.email.routing.key}")
+    private String emailRoutingKey;
+
     @Bean
     public Queue orderQueue(){
         return new Queue(orderQueue);
+    }
+
+    @Bean
+    public Queue emailQueue(){
+        return new Queue(emailQueue);
     }
 
     @Bean
@@ -37,6 +48,14 @@ public class RabbitMQConfig {
                 .bind(orderQueue())
                 .to(exchange())
                 .with(orderRoutingKey);
+    }
+
+    @Bean
+    public Binding emailBinding(){
+        return BindingBuilder
+                .bind(emailQueue())
+                .to(exchange())
+                .with(emailRoutingKey);
     }
 
     @Bean
